b42d17c3897c2611b03faa0dfc2594ea1c91b55e
yubingjiaocn <yubingjiaocn@gmail.com>
2021-08-03 21:40:50 +0800

    Add multithread support

--- liveme_s3_merger/app.py
+++ liveme_s3_merger/app.py
@@ -1,20 +1,27 @@
 import json
 import boto3
+import botocore
 import os
 import logging
+import threading
 from natsort import natsorted
 
 logging.basicConfig(format='%(asctime)s => %(message)s')
 
 BUCKET = ''
 MIN_S3_SIZE = 6000000
+parts_mapping = []
 
 
 def new_s3_client():
     # initialize an S3 client with a private session so that multithreading
     # doesn't cause issues with the client's internal state
+    client_config = botocore.config.Config(
+        max_pool_connections=25,
+    )
+
     session = boto3.session.Session()
-    return session.client('s3')
+    return session.client('s3', config=client_config)
 
 def collect_parts(s3, folder, prefix):
     return list(filter(lambda x: x[0].startswith(folder+"/"+prefix), _list_all_objects_with_size(s3, folder)))   
@@ -100,39 +107,48 @@ def initiate_concatenation(s3, result_filename):
     return resp['UploadId']
 
 def assemble_parts_to_concatenate(s3, result_filename, upload_id, parts_list):
+    global parts_mapping 
     parts_mapping = []
     part_num = 0
     grouped_list = chunk_by_size(parts_list, MIN_S3_SIZE)
-
+    threads = []
     for group in grouped_list:
         part_num = part_num + 1
-        if len(group) == 1:
-            resp = s3.upload_part_copy(Bucket=BUCKET,
-                               Key=result_filename,
-                               PartNumber=part_num,
-                               UploadId=upload_id,
-                               CopySource=BUCKET+"/"+group[0][0])
-            logging.warning("Setup S3 part #{}, with path: {}, and got response: {}".format(part_num, group[0][0], resp))
-            parts_mapping.append({'ETag': resp['CopyPartResult']['ETag'][1:-1], 'PartNumber': part_num})    
-        else: 
-            small_parts = []
-            for source_part in group:
-                temp_filename = "/tmp/{}".format(source_part[0].replace("/","_"))
-                s3.download_file(Bucket=BUCKET, Key=source_part[0], Filename=temp_filename)
-    
-                with open(temp_filename, 'rb') as f:
-                    small_parts.append(f.read())
-                os.remove(temp_filename)
-                logging.warning("Downloaded and copied small part with path: {}".format(source_part[0]))
-    
-            if len(small_parts) > 0:
-                part = bytes()
-                part = bytes().join(small_parts)
-                resp = s3.upload_part(Bucket=BUCKET, Key=result_filename, PartNumber=part_num, UploadId=upload_id, Body=part)
-                logging.warning("Setup local part #{} from {} small files, and got response: {}".format(part_num, len(small_parts), resp))
-                parts_mapping.append({'ETag': resp['ETag'][1:-1], 'PartNumber': part_num})
-    
+        t = threading.Thread(target=_thread_concatenate,
+                                 args=(group, part_num, s3, result_filename, upload_id))
+        threads.append(t)
+        t.start()
+
+    for t in threads:
+        t.join()    
     return parts_mapping
+
+def _thread_concatenate(group, part_num, s3, result_filename, upload_id):
+    global parts_mapping
+    if len(group) == 1:
+        resp = s3.upload_part_copy(Bucket=BUCKET,
+                           Key=result_filename,
+                           PartNumber=part_num,
+                           UploadId=upload_id,
+                           CopySource=BUCKET+"/"+group[0][0])
+        logging.warning("Setup S3 part #{}, with path: {}, and got response: {}".format(part_num, group[0][0], resp))
+        parts_mapping.append({'ETag': resp['CopyPartResult']['ETag'][1:-1], 'PartNumber': part_num})    
+    else: 
+        small_parts = []
+        for source_part in group:
+            temp_filename = "/tmp/{}".format(source_part[0].replace("/","_"))
+            s3.download_file(Bucket=BUCKET, Key=source_part[0], Filename=temp_filename)
+
+            with open(temp_filename, 'rb') as f:
+                small_parts.append(f.read())
+            os.remove(temp_filename)
+            logging.warning("Downloaded and copied small part with path: {}".format(source_part[0]))
+
+        if len(small_parts) > 0:
+            part = bytes().join(small_parts)
+            resp = s3.upload_part(Bucket=BUCKET, Key=result_filename, PartNumber=part_num, UploadId=upload_id, Body=part)
+            logging.warning("Setup local part #{} from {} small files, and got response: {}".format(part_num, len(small_parts), resp))
+            parts_mapping.append({'ETag': resp['ETag'][1:-1], 'PartNumber': part_num})
 
 def complete_concatenation(s3, result_filename, upload_id, parts_mapping):
     if len(parts_mapping) == 0:
@@ -140,6 +156,7 @@ def complete_concatenation(s3, result_filename, upload_id, parts_mapping):
         logging.warning("Aborted concatenation for file {}, with upload id #{} due to empty parts mapping".format(result_filename, upload_id))
         raise
     else:
+        print(format(parts_mapping))
         resp = s3.complete_multipart_upload(Bucket=BUCKET, Key=result_filename, UploadId=upload_id, MultipartUpload={'Parts': parts_mapping})
         logging.warning("Finished concatenation for file {}, with upload id #{}, and parts mapping: {}".format(result_filename, upload_id, parts_mapping))
         return result_filename
